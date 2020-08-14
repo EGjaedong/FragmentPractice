61184fbf6ca4bc1fc3fe244ab29cdc8e47fad058
unknown <hzh_innovation@163.com>
2020-08-14 23:43:15 +0800

    [hzh] feat: add buttons fragment layout to main layout

--- app/src/main/java/com/thoughtworks/fragment/MainActivity.java
+++ app/src/main/java/com/thoughtworks/fragment/MainActivity.java
@@ -10,5 +10,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ButtonsFragment buttonsFragment = new ButtonsFragment();
+        getSupportFragmentManager().beginTransaction().add(R.id.buttons_container, buttonsFragment).commit();
     }
 }
